a4c9b6de4265097c62dc39e897875d66b26772b1
Caeden Statia <caeden.s@outlook.com>
2018-04-25 17:49:11 -0700

    Created some internal functions that allow for easy reading/writing off of files and easy conversion to/from files and Member structs.

--- Eclipse/Characters.cs
+++ Eclipse/Characters.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Eclipse
 {
@@ -30,12 +32,63 @@ namespace Eclipse
             "Angela"
         };
 
-        string test = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "test.bob"); //Saving this for later.
-        //https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/file-system/how-to-create-a-file-or-folder information about creating folders and stuff.
-
+        /// <summary>
+        /// Returns a new Member based on random stats, with a unique name.
+        /// </summary>
         public Member newMember()
         {
-            return new Member(nameCombinations[rng.Next(0, nameCombinations.Length)]);
+            //Removes any names already used before choosing a random one.
+            List<string> memberNames = new List<string>(nameCombinations);
+            string folder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), string.Format("EclipseGame\\{0}#{1}", Properties.Settings.Default.Name, Properties.Settings.Default.identifier));
+            foreach (string file in Directory.EnumerateFiles(folder))
+            {
+                memberNames.Remove(file.Split('\\')[file.Split('\\').Length].Split('.')[0]);
+            }
+            return new Member(memberNames[rng.Next(0, memberNames.Count)]);
+        }
+
+        /// <summary>
+        /// An empty Member object.
+        /// </summary>
+        static Member Empty = new Member("", 0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+        /// <summary>
+        /// Converts a .ecm file found in the game data that matches the name given to a Member struct. Returns Characters.Empty if none exists.
+        /// </summary>
+        public Member fromFile(string characterName) {
+            string folder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), string.Format("EclipseGame\\{0}#{1}", Properties.Settings.Default.Name, Properties.Settings.Default.identifier));
+            try
+            {
+                string[] file = File.ReadAllLines(Path.Combine(folder, characterName + ".ecm"));
+                return new Member(file[0], int.Parse(file[1]), int.Parse(file[2]), int.Parse(file[3]), int.Parse(file[4]), int.Parse(file[5]), int.Parse(file[6]), int.Parse(file[7]), int.Parse(file[8]), int.Parse(file[9]));
+            }
+            catch (Exception)
+            {
+                return Empty;
+            }
+        }
+
+        /// <summary>
+        /// Takes a Member struct and writes it to the disk as a .ecm file. Returns the file location as a string.
+        /// WARNING: Automatically overwrites any preexisting file.
+        /// </summary>
+        public string toFile(Member member)
+        {
+            string folder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), string.Format("EclipseGame\\{0}#{1}", Properties.Settings.Default.Name, Properties.Settings.Default.identifier));
+            using (StreamWriter fs = new StreamWriter(File.Create(Path.Combine(folder, member.name + ".ecm"))))
+            {
+                fs.WriteLine(member.name);
+                fs.WriteLine(member.armor);
+                fs.WriteLine(member.healthMax);
+                fs.WriteLine(member.health);
+                fs.WriteLine(member.strength);
+                fs.WriteLine(member.dexterity);
+                fs.WriteLine(member.agility);
+                fs.WriteLine(member.constitution);
+                fs.WriteLine(member.level);
+                fs.WriteLine(member.disposition);
+            }
+            return Path.Combine(folder, member.name + ".ecm");
         }
     }
 
@@ -50,6 +103,7 @@ namespace Eclipse
         public int agility;
         public int constitution;
         public int level;
+        public int disposition;
         public Member(string memberName)
         {
             Random rng = new Random();
@@ -61,8 +115,9 @@ namespace Eclipse
             constitution = rng.Next(5, 20);
             healthMax = health = 15;
             armor = level + ((strength - 10) / 2);
+            disposition = 0;
         }
-        public Member(string a, int b, int c, int d, int e, int f, int g, int h, int i)
+        public Member(string a, int b, int c, int d, int e, int f, int g, int h, int i, int k)
         {
             level = i;
             name = a;
@@ -73,6 +128,7 @@ namespace Eclipse
             health = d;
             healthMax = c;
             armor = b;
+            disposition = k;
         }
     }
 
